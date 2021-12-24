b6837355da6f5e6d7dcfcc978063cf4dbfd025db
Cihat Yildiz <cihat.yildiz@gmail.com>
2021-12-24 15:16:36 -0800

    Update FortifySSCToken.py

--- dr-octo/dr-octo/scripts/FortifySSCToken.py
+++ dr-octo/dr-octo/scripts/FortifySSCToken.py
@@ -21,47 +21,4 @@ if __name__ == '__main__':
   token = getForitfySSCToken()
   del os.environ['FORTIFY_SSC_UNIFIED_LOGIN_TOKEN']
   os.environ['FORTIFY_SSC_UNIFIED_LOGIN_TOKEN'] = 'token'
-
-
-
-# # Use at end of script to delete token
-# def del_token(tkn_id):
-#     headers = {
-#         'Accept': "application/json",
-#         'Content-Type': "application/json",
-#         'Authorization': "Basic " + b64auth
-#     }
-#     conn.request("DELETE", ssc_cntx + "api/v1/tokens/" + tkn_id, headers=headers)
-#     res = conn.getresponse()
-#     data = res.read()
-#     parsed_data = json.loads(data.decode("utf-8"))
-#     response_code = parsed_data["responseCode"]
-#     if response_code == 200:
-#         print("Token deleted successfully")
-#     else:
-#         print("Failed to delete token")
-
-
-# #Download the source code for Fotify Scan
-# def download_code():
-#     print("Downloading the code for Fortify scan \n")
-#     app_name="hellogitworld"
-#     os.system("rm -rf /opt/Fortify/source_code/hellogitworld")
-#     cmd="git clone https://github.com/githubtraining/hellogitworld.git ./source_code/"+app_name
-#     print(cmd)
-#     os.system(cmd)
-#     return(app_name)
-
-# def get_token():
-#     payload = "{\"type\": \"UnifiedLoginToken\",\"description\":\"REST API token for testing\"}"
-#     headers = {
-#         'Accept': "application/json",
-#         'Content-Type': "application/json",
-#         'Authorization': "Basic " + b64auth
-#     }
-#     conn.request("POST", ssc_cntx + "api/v1/tokens", payload, headers)
-#     res = conn.getresponse()
-#     data = res.read()
-#     parsed_data = json.loads(data.decode("utf-8"))
-#     return {"id": parsed_data["data"]["id"], "token": parsed_data["data"]["token"]}
-
+  
