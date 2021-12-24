4ba254c92221780fa4b3fa4590d77c780bbf04ce
Cihat Yildiz <cihat.yildiz@gmail.com>
2021-12-24 15:13:02 -0800

    Update ConfigureFortifyServer.sh

--- dr-octo/dr-octo/scripts/fortify/ConfigureFortifyServer.sh
+++ dr-octo/dr-octo/scripts/fortify/ConfigureFortifyServer.sh
@@ -1,6 +1,6 @@
-export SSC_USERNAME=username
-export SSC_PASSWORD=passwd
-export GIT_TOKEN=<git-token>
+export SSC_USERNAME=
+export SSC_PASSWORD=
+export GIT_TOKEN=
 export GraphQL_URL=https://<>graphql-server/graphql
 yum -y install rh-python36 rh-python36-python-tools rh-python36-python-six
 scl enable rh-python36 bash
